f57807d75ee6b17c9b1b128a8b1f5183f0a088d5
jgeis <jgeis@VENGEANCE>
2021-05-02 22:08:31 -0400

    added connector and logger

--- bank-app/src/main/java/dao/DbConnector.java
+++ bank-app/src/main/java/dao/DbConnector.java
@@ -1,5 +1,39 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import utility.ConfigReader;
+
 public class DbConnector {
+	
+	private static final Logger logger = LogManager.getLogger(DbConnector.class);
+	
+	private static DbConnector instance;
+	private Connection conn = null;
+	
+	private DbConnector() throws Exception {
+		
+			String url = ConfigReader.getInstance().getProperty("DB_URL");
+			String user = ConfigReader.getInstance().getProperty("DB_USER");
+			String password = ConfigReader.getInstance().getProperty("DB_PASSWORD");
+			
+			this.conn = DriverManager.getConnection(url, user, password);
+			
+	}
+	
+	public static DbConnector getInstance() throws Exception {
+		if(instance == null) {
+			instance = new DbConnector();
+		}
+		return instance;
+	}
+	
+	public Connection getConnection() {
+		return conn;
+	}
 
 }
