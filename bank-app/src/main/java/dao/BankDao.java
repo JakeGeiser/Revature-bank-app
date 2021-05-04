ccd86d7d9f7be0f8e61fc5b915807b269c4eacb6
jgeis <jgeis@VENGEANCE>
2021-05-04 14:59:47 -0400

    added insertCustomer method

--- bank-app/src/main/java/dao/BankDao.java
+++ bank-app/src/main/java/dao/BankDao.java
@@ -1,16 +1,53 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import model.Customer;
 
 /**
  * Data Access Object for Bank App
  * @author Jake Geiser
  */
 public class BankDao {
-	// create connection
+	// initialize logger
+	private static final Logger logger = LogManager.getLogger(DbConnector.class);
 	
-	//// register user
+	// insert customer method
+	public boolean insertCustomer(Customer customer) throws Exception{
+		int inserted = 0;
+		try {
+			
+			logger.debug("Received customer to register: "+customer);
+			
+			Connection conn = DbConnector.getInstance().getConnection();
+			String sql = "INSERT INTO bank.customer(first_name, last_name, email, password, phone, join_date)"
+							+ "'?', '?', '?', '?', '?' , CURRENT_TIMESTAMP";
+			logger.debug("using statement", sql);
+			
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, customer.getFirstName());
+			pstmt.setString(2, customer.getLastName());
+			pstmt.setString(3, customer.getEmail());
+			pstmt.setString(4, customer.getPassword());
+			pstmt.setString(5, customer.getPhone());
+			
+			inserted = pstmt.executeUpdate();
+			logger.debug("Inserted customer: "+inserted);
+			return inserted != 0;
+		} catch (SQLException e) {
+			logger.error("Unable to insert customer into bank.customer", e);
+			return false;
+		}
+		
+		
+	}
 	
 	// get Customer information
 	public ArrayList<Object> getCustomer() throws Exception{
