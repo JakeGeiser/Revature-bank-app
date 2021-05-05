87a819f9536e4e394c83ffdcd38ce9290ce3239a
jgeis <jgeiser@uncc.edu>
2021-05-05 19:54:21 -0400

    added show Options methods to BankUI

--- bank-app/src/main/java/client/BankUI.java
+++ bank-app/src/main/java/client/BankUI.java
@@ -42,5 +42,61 @@ public class BankUI { // Customer Layer
 	private static void run(Scanner input) throws ItemNotFoundException, Exception{
 		
 	}
-
+	
+	
+	
+	
+	//// show methods for printing options
+	// login - initial homepage options
+	private static void showLoginOptions() {
+		System.out.println();
+		System.out.println("Bank Log In Options");
+		System.out.println("====================");
+		System.out.println("1. User Log In");
+		System.out.println("2. Register New User");
+		System.out.println("3. Employee Log In");
+		System.out.println("4. Exit Application");
+	}
+	// show customer options once logged in
+	private static void showCustomerOptions() {
+		System.out.println();
+		System.out.println("User Options");
+		System.out.println("===========================");
+		System.out.println("1. Select Account Portal");
+		System.out.println("2. Account Balance Transfer");
+		System.out.println("3. Log Out");
+	}
+	// show customer account options in account portal
+	private static void showAccountOptions() {
+		System.out.println();
+		System.out.println("Account Options");
+		System.out.println("===============================");
+		System.out.println("1. Display Account Balance");
+		System.out.println("2. Display Account Transactions");
+		System.out.println("3. Deposit");
+		System.out.println("4. Withdraw");
+		System.out.println("5. Return to User Options Page");
+	}
+	// show employee options once logged in
+	private static void showEmployeeOptions() {
+		System.out.println();
+		System.out.println("Employee Options");
+		System.out.println("===============================");
+		System.out.println("1. Display Customers");
+		System.out.println("2. Show Customer Accounts by ID");
+		System.out.println("3. Show All Transactions");
+		System.out.println("4. Enter Account Requests Portal");
+		System.out.println("5. Log Out");
+	}
+	// show all account request options in Account Requests Portal
+	private static void showAccountRequestOptions() {
+		System.out.println();
+		System.out.println("Account Request Options");
+		System.out.println("===============================");
+		System.out.println("1. Approve Account Request");
+		System.out.println("2. Deny Account Request");
+		System.out.println("3. Show All Account Requests");
+		System.out.println("4. Show All Pending Account Requests");
+		System.out.println("5. Return to Employee Options");	
+	}
 }
