96b69155fe0eac0a44d5de85c18f9351fa6959d3
jgeis <jgeiser@uncc.edu>
2021-05-05 17:17:58 -0400

    added basic structure to BankUI

--- bank-app/src/main/java/client/BankUI.java
+++ bank-app/src/main/java/client/BankUI.java
@@ -1,19 +1,43 @@
 package client;
 
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import exceptions.ItemNotFoundException;
+
 /**
  * User Interface for Bank Application
  * @author Jake Geiser
  */
 
 public class BankUI { // Customer Layer
+	
+	private static final Logger logger = LogManager.getLogger(BankUI.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		logger.info("Application Started");
 		
-		// Select specific account to view (done on UI level)
+		System.out.println("Welcome to Revature Banking");
 		
-		// verify if user doesn't already exist and insert if available
+		Scanner input = new Scanner(System.in);
+		
+		try {
+			run(input);
+		} catch (ItemNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// method to handle user input and handle interactions with BankManger
+	private static void run(Scanner input) throws ItemNotFoundException, Exception{
+		
 	}
 
 }
